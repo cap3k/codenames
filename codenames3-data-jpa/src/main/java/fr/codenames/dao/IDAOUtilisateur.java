955c7c052582fdc3d5f0a1dc2628238be8371d2b
Marie <marie.magnaudeix@soprasteria.com>
2019-01-10 14:42:52 +0100

    Création de la connexion de la personne

--- codenames3-data-jpa/src/main/java/fr/codenames/dao/IDAOUtilisateur.java
+++ codenames3-data-jpa/src/main/java/fr/codenames/dao/IDAOUtilisateur.java
@@ -2,15 +2,16 @@ package fr.codenames.dao;
 
 import fr.codenames.exception.UsernameOrPasswordNotFoundException;
 import fr.codenames.model.Utilisateur;
-
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.repository.query.Param;
 
 import fr.codenames.exception.AccountLockedException;
+
 @NoRepositoryBean
-public interface IDAOUtilisateur<T> extends JpaRepository<Utilisateur, Integer> {
+public interface IDAOUtilisateur<T extends Utilisateur> extends JpaRepository<T, Integer> {
 	/**
 	 * Methode d'authentification pour un utilisateur (Joueur ou Administrateur)
 	 * @param username
@@ -19,13 +20,16 @@ public interface IDAOUtilisateur<T> extends JpaRepository<Utilisateur, Integer> 
 	 * @throws UsernameOrPasswordNotFoundException
 	 * @throws AccountLockedException
 	 */
-	public T auth(String username, String password) throws UsernameOrPasswordNotFoundException, AccountLockedException;
+	
+	
+	@Query ("select j from #{#entityName} j where j.username = :userName and j.password = :pwd")
+	public T auth(@Param("userName") String username, @Param("pwd")String password) throws UsernameOrPasswordNotFoundException, AccountLockedException;
 
-	public void banById(int nextInt);
-
-	List<T> findAllBan();
-
-	List<T> findAllUnBan();
-
-	void unBanById(int id);
+//	public void banById(int nextInt);
+//
+//	List<T> findAllBan();
+//
+//	List<T> findAllUnBan();
+//
+//	void unBanById(int id);
 }
--- codenames3-java/src/main/java/fr/codenames/Application.java
+++ codenames3-java/src/main/java/fr/codenames/Application.java
@@ -1,18 +1,57 @@
 package fr.codenames;
 
+import java.util.Scanner;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import fr.codenames.dao.IDAOCarte;
+import fr.codenames.dao.IDAOJoueur;
 import fr.codenames.dao.IDAOUtilisateur;
+import fr.codenames.exception.AccountLockedException;
+import fr.codenames.exception.UsernameOrPasswordNotFoundException;
+import fr.codenames.model.Joueur;
+import fr.codenames.model.TypeUtilisateur;
+import fr.codenames.model.Utilisateur;
 
 public class Application {
 
 	@Autowired
 	private IDAOCarte myCarte;
+	@Autowired
+	private IDAOJoueur myUtilisateur;
+	@Autowired
+	private IDAOJoueur myJoueur;
+	private Scanner sc;
 
 	public void run(String[] args) {
 
 		System.out.println(myCarte.findById(1).get().getLibelle());
+		System.out.println(myUtilisateur.findById(2).get().getNom());
 
+		connexion();
+	}
+
+	public void connexion() {
+		sc = new Scanner(System.in);
+
+		System.out.print("Indiquer le nom d'utilisateur : ");
+		String username = sc.nextLine();
+
+		System.out.print("Indiquer le mot de passe : ");
+		String password = sc.nextLine();
+
+		try {
+			System.out.println("lancement authentification");
+			myJoueur.auth(username, password);
+			System.out.println("connecte!");
+		}
+
+		catch (UsernameOrPasswordNotFoundException e) {
+			System.out.println("MAUVAIS USERNAME OU PASSWORD !!");
+		}
+
+		catch (AccountLockedException e) {
+			System.out.println("COMPTE BLOQUE ... SORRY !");
+		}
 	}
 }
