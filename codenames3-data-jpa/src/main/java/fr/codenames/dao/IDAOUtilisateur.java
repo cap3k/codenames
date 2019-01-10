582b05dd3ef609ff0f2d45aa1a281e99ca11f734
dydy <dylanliner@gmail.com>
2019-01-10 11:11:28 +0100

    Pom.xlm+Interfaces

---
+++ codenames3-data-jpa/src/main/java/fr/codenames/dao/IDAOUtilisateur.java
@@ -0,0 +1,30 @@
+package fr.codenames.dao;
+
+import fr.codenames.exception.UsernameOrPasswordNotFoundException;
+import fr.codenames.model.Utilisateur;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import fr.codenames.exception.AccountLockedException;
+
+public interface IDAOUtilisateur extends JpaRepository {
+	/**
+	 * Methode d'authentification pour un utilisateur (Joueur ou Administrateur)
+	 * @param username
+	 * @param password
+	 * @return
+	 * @throws UsernameOrPasswordNotFoundException
+	 * @throws AccountLockedException
+	 */
+	public Utilisateur auth(String username, String password) throws UsernameOrPasswordNotFoundException, AccountLockedException;
+
+	public void banById(int nextInt);
+
+	List<Utilisateur> findAllBan();
+
+	List<Utilisateur> findAllUnBan();
+
+	void unBanById(int id);
+}
