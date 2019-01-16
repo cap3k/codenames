0a7d70c8da54fc0f3ac3e92257541caeb1def0a2
Marie <marie.magnaudeix@soprasteria.com>
2019-01-16 17:02:19 +0100

    mise à jour gestion cartes
    reste à faire : suppression et boutons modif/suppression dans la liste des cartes

--- codenames-3-web/WebContent/assets/js/cartes.js
+++ codenames-3-web/WebContent/assets/js/cartes.js
@@ -2,15 +2,13 @@ function createRowCarte(carte){
 	//CREATION DE COLONNES
 	var myColId = $('<td>' + carte.id + '</td>');
 	var myColLibelle = $('<td>' + carte.libelle + '</td>');
-	var myColChoix = $('<td> <a class="btn btn-primary" href="#modifierCarte">modifier</a></td>');
-	//<a class="btn btn-primary" href="#inscription">Inscription</a>
+	
 	//CREATION DE LA LIGNE
 	var myLigne = $('<tr />');
 	
 	//ASSOCIER LES COLONNES A LA LIGNE
 	myLigne.append(myColId);
 	myLigne.append(myColLibelle);
-	myLigne.append(myColChoix);
 	
 	//INSERER LA LIGNE AU TABLEAU
 	$('table tbody').append(myLigne);
@@ -21,19 +19,8 @@ function createListeDeroulanteCarte(carte){
 	var myId = $('<option value="'+ carte.id + '">' + carte.libelle + '</option>');
 	
 	//INSERER LA LIGNE AU TABLEAU
-	$('select[id="nomCarte"]').append(myId);
+	$('select[class="form-control"]').append(myId);
 }
-
-$.ajax({
-	method : 'GET',
-	url : 'http://192.168.1.110/codenames-ajax/carte',
-	success : function(cartes) {
-		for(let carte of cartes){
-			createRowCarte(carte);
-			createListeDeroulanteCarte(carte);
-		}
-	}
-});
 
 function ajouterCarte(){
 	var maCarte = {
@@ -48,6 +35,7 @@ function ajouterCarte(){
 		contentType : 'application/json',	//DE QUOI EST FAIT LE FLUX
 		success : function(carte) {		//LA REPONSE DU SERVEUR
 			createRowCarte(carte);
+			createListeDeroulanteCarte(carte);
 		}
 	});
 }
@@ -65,15 +53,35 @@ function modifierCarte(){
 		contentType : 'application/json',	//DE QUOI EST FAIT LE FLUX
 		success : function(carte) {		//LA REPONSE DU SERVEUR
 			createRowCarte(carte);
+			createListeDeroulanteCarte(carte);
 		}
 	});
 }
 
-$('table[id="cartes"]').bind('click', function() {
-//	alert("A MALIBU");
-//	this -> lien cliqué
-//	alert($(this).attr('href'));
-	alert("test");
-	$($(this).attr('td a')).show();
-	return false;
-})
+function supprimerCarte(){
+	var maCarte = {
+		id: $('select[name="nomCarte"]').val()
+	}
+	//REQUETE AJAX POUR AJOUTER LE PRODUIT
+	$.ajax({
+		method : 'DELETE',
+		url : 'http://192.168.1.110/codenames-ajax/carte/#' + maCarte.id,
+		data : JSON.stringify(maCarte),	//CONVERTIR L'OBJET JS EN JSON
+		contentType : 'application/json',	//DE QUOI EST FAIT LE FLUX
+		success : function(carte) {		//LA REPONSE DU SERVEUR
+			createRowCarte(carte);
+			createListeDeroulanteCarte(carte);
+		}
+	});
+}
+
+$.ajax({
+	method : 'GET',
+	url : 'http://192.168.1.110/codenames-ajax/carte',
+	success : function(cartes) {
+		for(let carte of cartes){
+			createRowCarte(carte);
+			createListeDeroulanteCarte(carte);
+		}
+	}
+});
