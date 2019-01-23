8d27a5bd14ed232e1998772cd64d8b26866e51f7
dydy <dylanliner@gmail.com>
2019-01-23 12:30:31 +0100

    creation codenames web2

---
+++ codenames-3-web2/src/main/java/fr/codenames/controller/HomeController.java
@@ -0,0 +1,5 @@
+package fr.codenames.controller;
+
+public class HomeController {
+
+}
---
+++ codenames-3-web2/src/main/java/fr/codenames/webConfiguration/WebConfig.java
@@ -0,0 +1,48 @@
+package fr.codenames.webConfiguration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+@Configuration
+@ComponentScan("fr.codenames")
+@EnableWebMvc
+
+public class WebConfig implements WebMvcConfigurer {
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+		registry.addResourceHandler("/images/**").addResourceLocations("/images/");
+		registry.addResourceHandler("/assets/**").addResourceLocations("/images/");
+	}
+		
+	@Bean
+	public SpringResourceTemplateResolver templateResolver() {
+	SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+	templateResolver.setPrefix("/WEB-INF/templates/");
+	templateResolver.setSuffix(".html");
+	return templateResolver;
+	}
+	
+	@Bean
+	public SpringTemplateEngine templateEngine(SpringResourceTemplateResolver templateResolver) {
+	SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+	templateEngine.setTemplateResolver(templateResolver);
+	templateEngine.setEnableSpringELCompiler(true);
+	return templateEngine;
+	}
+	
+	@Bean
+	public ThymeleafViewResolver viewResolver(SpringTemplateEngine templateEngine) {
+	ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+	viewResolver.setTemplateEngine(templateEngine);
+	return viewResolver;
+	}
+}
