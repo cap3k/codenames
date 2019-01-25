769a66a33e30173bce2fe2e1c805f5fc7fd7cb54
Marie <marie.magnaudeix@soprasteria.com>
2019-01-25 09:16:26 +0100

    Gestion de la connexion + création d'une page d'accueil avec règles
    connexion joueur et connexion utilisateur

--- codenames-3-web2/src/main/java/fr/codenames/controller/AdminController.java
+++ codenames-3-web2/src/main/java/fr/codenames/controller/AdminController.java
@@ -4,12 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
-@RequestMapping("/accueil")
-public class AccueilController {
+@RequestMapping("/admin")
+public class AdminController {
+	
 	@GetMapping({ "" })
-	public String accueil() {
-		return "accueil";
+	public String admin() {
+		return "admin";
 	}
+	
 }
---
+++ codenames-3-web2/src/main/java/fr/codenames/controller/ConnexionAdminController.java
@@ -0,0 +1,61 @@
+package fr.codenames.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import fr.codenames.dao.IDAOAdministrateur;
+
+
+@Controller
+@RequestMapping("/connexionAdmin")
+public class ConnexionAdminController {
+	
+	@Autowired
+	private IDAOAdministrateur daoAdmin;
+	
+	@GetMapping({ "" })
+	public String connexionAdm() {
+		return "connexionAdmin";
+	}
+	
+	@PostMapping("")
+	public String connexionAdm(@RequestParam String userNameAdm, @RequestParam String pwdAdm, Model model) {
+		model.addAttribute("administrateur", daoAdmin.auth(userNameAdm, pwdAdm));
+//VERIFICATION RENSIGNEMENT CHAMPS DE CONNEXION	
+		if (userNameAdm.equals("")) {
+			//username à renseigner
+			String msgUser;
+			msgUser="Veuillez rensigner un nom d'utilisateur";
+			model.addAttribute("msgUser", msgUser);
+			if (pwdAdm.equals("")) {
+				//mot de passe à renseigner
+				String msgPwd;
+				msgPwd="Veuillez rensigner un mot de passe";
+				model.addAttribute("msgPwd", msgPwd);
+				return "connexionAdmin";
+			}
+			else {
+				//username à renseigner
+				return "connexionAdmin";
+			}
+		}		
+		
+//CONNEXION	
+		if(daoAdmin.auth(userNameAdm, pwdAdm) == null) {
+			//mauvais username ou password
+			String msgWrong;
+			msgWrong="Le nom d'utilisateur ou le mot de passe est incorrect";
+			model.addAttribute("msgWrong", msgWrong);
+			return "connexionAdmin";
+		}
+		else {
+			return "redirect:./admin";
+		}
+	}
+}
