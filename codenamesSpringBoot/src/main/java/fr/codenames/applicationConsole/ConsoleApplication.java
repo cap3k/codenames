29f5b3dc141bb9790b36683750e434bb77ef45f0
Marie <marie.magnaudeix@soprasteria.com>
2019-01-29 16:43:01 +0100

    Connexion validée pour utilisateur et administrateur

---
+++ codenamesSpringBoot/src/main/java/fr/codenames/applicationConsole/ConsoleApplication.java
@@ -0,0 +1,19 @@
+package fr.codenames.applicationConsole; 
+ 
+ 
+import org.springframework.boot.CommandLineRunner; 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder; 
+import org.springframework.stereotype.Service; 
+ 
+ 
+@Service 
+public class ConsoleApplication implements CommandLineRunner { 
+ 
+	@Override 
+	public void run(String... args) throws Exception { 
+		BCryptPasswordEncoder bcrypt = new BCryptPasswordEncoder(); 
+	 
+		System.out.println(bcrypt.encode("mdp")); 
+ 
+	} 
+}
