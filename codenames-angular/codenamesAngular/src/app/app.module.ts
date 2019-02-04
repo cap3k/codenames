04ddb309c581db002cef9c7c254ee49a6329220d
dydy <dylanliner@gmail.com>
2019-02-04 15:09:21 +0100

    app.module

--- codenames-angular/codenamesAngular/src/app/app.module.ts
+++ codenames-angular/codenamesAngular/src/app/app.module.ts
@@ -5,9 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlateauComponent } from './plateau/plateau.component';
 import { AppConfigService } from './app-config.service';
+
 import {HttpClientModule} from '@angular/common/http';
 import { JeuComponent } from './jeu/jeu.component';
 import { ChatComponent } from './chat/chat.component';
+
+import { HttpClientModule } from '@angular/common/http';
+
 
 const routes: Routes = [
     { path: 'plateau/:id', component: PlateauComponent },
